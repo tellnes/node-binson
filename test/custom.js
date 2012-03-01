9bf7c4a968ddbd48a877a55cc484edcbbb3775c3
Christian Tellnes <christian@tellnes.no>
2012-03-01 19:34:59 +0100

    remove bad tests

--- test/custom.js
+++ test/custom.js
@@ -15,33 +15,6 @@ binson.register(0, {
 });
 
 describe('Custom', function(){
-  describe('encode', function(){
-    it('should equals hard coded buffer', function() {
-      var buffer = binson.encode(new Custom(2));
-      var ok = new Buffer(6);
-      buffer[0].should.equal(0xc4);
-      buffer[1].should.equal(0x24);
-      buffer[2].should.equal(0x10);
-      buffer[3].should.equal(0x69);
-      buffer[4].should.equal(0x64);
-      buffer[5].should.equal(0xe0);
-    });
-  });
-  describe('decode', function(){
-    it('should equals custom type 2', function() {
-      var custom = new Custom(2);
-      var buffer = new Buffer(6);
-
-      buffer[0] = 0xc4;
-      buffer[1] = 0x24;
-      buffer[2] = 0x10;
-      buffer[3] = 0x69;
-      buffer[4] = 0x64;
-      buffer[5] = 0xe0;
-
-      binson.decode(buffer).id.should.equal(custom.id);
-    });
-  });
   describe('encode-decode', function(){
     it('should equials orginale object', function(){
       var custom = new Custom(2);
